7e1f76781d9978886689067750462be25bb6bcdc
Sami BEN ELHAJ SALAH <sami.bhs.91@gmail.com>
2020-03-21 17:53:11 +0100

    second commit

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main()
 {  
     srand(time(0));
 
-    RenderWindow window(VideoMode(w, h), "Snake Game!");
+    RenderWindow window(VideoMode(w, h), "Snake Game by Sami !");
 
     Texture t1,t2;
     t1.loadFromFile("images/white.png");
